cd8686898028751b84ba252026ec8fe2a0deb2a5
Enrico <enricofarina667@gmail.com>
2024-08-23 11:05:18 -0300

    creating initial home

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Sssnake. Aumente agora a sua cobrinha!
-    </h1>
+    <div className='flex flex-col h-full w-11/12 items-center justify-center'>
+      <div className='flex flex-row items-start'>
+        <p className='text-white'>Sssnake</p>
+        <p className='text-white'>jogar/tutorial/créditos</p>
+      </div>
+      <div className="text-3xl font-bold w-11/12 h-4/5 bg-emerald-400">
+        Sssnake. Aumente agora a sua cobrinha!
+      </div>
+      <div className='teste'>
+
+      </div>
+    </div>
   )
 }
 
